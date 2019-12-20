038f7dd2944132b64fa78e9e46b9b978d1d24de1
Ranjeet jha <ranjeetrd2373@gmail.com>
2019-12-20 23:13:18 +0530

    FILE HANDLING

---
+++ FileHandling/src/com/ustglobal/filehandling/FileReadingClass.java
@@ -0,0 +1,24 @@
+package com.ustglobal.filehandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingClass {
+	
+	public static void main(String[] args) {
+		
+		String path = "C:\\Users\\Ranjeet\\Desktop\\text.txt";
+		FileReader reader = null;
+		try {
+			 reader = new FileReader(path);
+			int i;
+			while((i = reader.read())!=-1) {
+				System.out.print((char)i);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
